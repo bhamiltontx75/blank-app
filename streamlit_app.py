c50fbcd690626efc67a7be4e089d72a23cb2ce3d
bhamiltontx75 <109837456+bhamiltontx75@users.noreply.github.com>
2025-07-08 04:23:55 +0000

    change

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Load data
 df = pd.read_csv("group4cleaneddata.csv", parse_dates=['booking_datetime'])
